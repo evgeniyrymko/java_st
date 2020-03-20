fad650343b4e05c677181156da324d46a3bbd55a
Evgeniy Rymko <evgeniyrymko@gmail.com>
2020-03-20 15:10:47 +0300

    Sweater app first commit

---
+++ serving-web-content/src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+    Iterable<Message> findByTag(String tag);
+
+}
