915b4a01776ad1a4a0ec35bdfb0b8dbc039440bf
evgeniyrymko <evgeniyrymko@gmail.com>
2020-04-07 13:48:42 +0300

    Sweater app bean validation and password encoding are added

--- Sweater app/src/main/java/com/example/sweater/controller/RegistrationController.java
+++ Sweater app/src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -5,10 +5,12 @@ import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -22,22 +24,26 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
-//        User userFromDb = userRepo.findByUsername(user.getUsername());
-
-        if (!userService.addUser(user)) {
-            model.put("message", "User exists!");
+    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
+        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+            model.addAttribute("passwordError", "Passwords are different!");
+        }
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errors);
             return "registration";
         }
-//        user.setActive(true);
-//        user.setRoles(Collections.singleton(Role.USER));
-//        userRepo.save(user);
+
+        if (!userService.addUser(user)) {
+            model.addAttribute("usernameError", "User exists!");
+            return "registration";
+        }
 
         return "redirect:/login";
     }
 
     @GetMapping("/activate/{code}")
-    public String activate(Model model, @PathVariable String code){
+    public String activate(Model model, @PathVariable String code) {
 
         boolean isActivated = userService.activateUser(code);
 
