01cde3452885a117ec5d574693fa141d8d418789
evgeniyrymko <evgeniyrymko@gmail.com>
2020-05-07 12:46:14 +0300

    #10 Angular Routing and Routes

--- NoteIt/noteit-ng-app/src/app/app.module.ts
+++ NoteIt/noteit-ng-app/src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {FeedbackComponent} from './feedback/feedback.component';
+import {NotesComponent} from './notes/notes.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+  {
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
