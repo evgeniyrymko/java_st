f5f3ce538b2c638b16b77cf5d9afc08fd118c1d0
evgeniyrymko <evgeniyrymko@gmail.com>
2020-05-11 11:17:23 +0300

    #14 Fetch Data Using HTTP in Angular

--- NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
+++ NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Notebook} from "./model/notebook";
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notebooks: Notebook[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.getAllNotebooks();
+  }
+
+  public getAllNotebooks() {
+    let url = "http://localhost:8082/api/notebooks/all";
+    this.http.get<Notebook[]>(url).subscribe(
+      res => {
+        this.notebooks = res;
+      },
+      err => {
+        alert("An error has occurred during getting all notebooks");
+      }
+    );
   }
 
 }
