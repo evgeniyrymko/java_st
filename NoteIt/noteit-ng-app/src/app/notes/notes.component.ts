6062f4a214be03b0a9025baaea96244601d25e89
evgeniyrymko <evgeniyrymko@gmail.com>
2020-05-12 12:49:29 +0300

    #16 Notebook management

--- NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
+++ NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
@@ -28,4 +28,43 @@ export class NotesComponent implements OnInit {
     );
   }
 
+  public createNotebook() {
+    let newNotebook: Notebook = {
+      name: 'New Notebook',
+      id: null,
+      nbNotes: 0
+    }
+    this.apiService.postNotebook(newNotebook).subscribe(
+      res => {
+        newNotebook.id = res.id;
+        this.notebooks.push(newNotebook);
+      },
+      err => {
+        alert("An error has occurred during adding new notebook");
+      }
+    );
+  }
+
+  public updateNotebook(updatedNotebook: Notebook) {
+    this.apiService.postNotebook(updatedNotebook).subscribe(
+      res=>{},
+      err=>{
+        alert("An error has occurred during updating (changing) the notebook");
+      }
+    );
+  }
+
+  public deleteNotebook(notebook: Notebook) {
+    if (confirm("Are you sure you want to delete the notebook?")){
+      this.apiService.deleteNotebook(notebook.id).subscribe(
+        res =>{
+          let indexOfNotebook = this.notebooks.indexOf(notebook);
+          this.notebooks.splice(indexOfNotebook,1);
+        },
+        err =>{
+          alert("An error has occurred during deleting the notebook");
+        }
+      );
+    }
+  }
 }
