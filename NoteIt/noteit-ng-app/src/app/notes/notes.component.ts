015de15cd188358f6afd740f853627231700f628
evgeniyrymko <evgeniyrymko@gmail.com>
2020-05-13 15:04:00 +0300

    #17 Notes management

--- NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
+++ NoteIt/noteit-ng-app/src/app/notes/notes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Notebook} from "./model/notebook";
 import {ApiService} from "../shared/api.service";
+import {Note} from "./model/note";
 
 @Component({
   selector: 'app-notes',
@@ -9,12 +10,15 @@ import {ApiService} from "../shared/api.service";
 })
 export class NotesComponent implements OnInit {
   notebooks: Notebook[] = [];
+  notes: Note[] = [];
+  selectedNotebook: Notebook;
 
   constructor(private apiService: ApiService) {
   }
 
   ngOnInit(): void {
     this.getAllNotebooks();
+    this.getAllNotes();
   }
 
   public getAllNotebooks() {
@@ -24,6 +28,17 @@ export class NotesComponent implements OnInit {
       },
       err => {
         alert("An error has occurred during getting all notebooks");
+      }
+    );
+  }
+
+  public getAllNotes() {
+    this.apiService.getAllNotes().subscribe(
+      res => {
+        this.notes = res;
+      },
+      err => {
+        alert("An error has occurred during getting all notes");
       }
     );
   }
@@ -47,24 +62,85 @@ export class NotesComponent implements OnInit {
 
   public updateNotebook(updatedNotebook: Notebook) {
     this.apiService.postNotebook(updatedNotebook).subscribe(
-      res=>{},
-      err=>{
+      res => {
+      },
+      err => {
         alert("An error has occurred during updating (changing) the notebook");
       }
     );
   }
 
   public deleteNotebook(notebook: Notebook) {
-    if (confirm("Are you sure you want to delete the notebook?")){
+    if (confirm("Are you sure you want to delete the notebook?")) {
       this.apiService.deleteNotebook(notebook.id).subscribe(
-        res =>{
+        res => {
           let indexOfNotebook = this.notebooks.indexOf(notebook);
-          this.notebooks.splice(indexOfNotebook,1);
+          this.notebooks.splice(indexOfNotebook, 1);
         },
-        err =>{
+        err => {
           alert("An error has occurred during deleting the notebook");
         }
       );
     }
+  }
+
+  public deleteNote(note: Note) {
+    if (confirm("Are you sure you want to delete this note?")) {
+      this.apiService.deleteNote(note.id).subscribe(
+        res => {
+          let indexOfNote = this.notes.indexOf(note);
+          this.notes.splice(indexOfNote, 1);
+        },
+        err => {
+          alert("An error has occurred during deleting the note");
+        },
+      );
+    }
+  }
+
+  public createNote(notebookId: string) {
+    let newNote: Note = {
+      id: null,
+      title: "New Note",
+      text: "Write here your text",
+      notebookId: notebookId,
+      lastModifiedOn: null
+    }
+    this.apiService.postNote(newNote).subscribe(
+      res => {
+        newNote.id = res.id;
+        this.notes.push(newNote);
+      },
+      err => {
+        alert("An error has occurred during creating a new note");
+      }
+    );
+  }
+
+  public selectNotebook(notebook: Notebook) {
+    this.selectedNotebook = notebook;
+    this.apiService.getNotesByNotebook(notebook.id).subscribe(
+      res => {
+        this.notes = res;
+      },
+      err => {
+        alert("An error has occurred during filtering the notes of selected notebook");
+      },
+    );
+  }
+
+  public updateNote(updatedNote: Note) {
+    this.apiService.postNote(updatedNote).subscribe(
+      res => {
+      },
+      err => {
+        alert("An error has occurred during updating the note");
+      }
+    );
+  }
+
+  public selectAllNotes() {
+    this.selectedNotebook = null;
+    this.getAllNotes();
   }
 }
--- NoteIt/noteit-ng-app/src/app/shared/api.service.ts
+++ NoteIt/noteit-ng-app/src/app/shared/api.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Notebook} from "../notes/model/notebook";
 import {FeedbackViewModel} from "../feedback/feedback.component";
+import {Note} from "../notes/model/note";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class ApiService {
   private SEND_FEEDBACK_URL = `${this.BASE_URL}/feedback`;
   private SAVE_UPDATE_NOTEBOOK_URL = `${this.BASE_URL}/notebooks`;
   private DELETE_NOTEBOOK_URL = `${this.BASE_URL}/notebooks/`;
+  private ALL_NOTES_URL = `${this.BASE_URL}/notes/all`;
+  private NOTES_BY_NOTEBOOK_URL = `${this.BASE_URL}/notes/byNotebook/`;
+  private SAVE_UPDATE_NOTE_URL = `${this.BASE_URL}/notes`;
+  private DELETE_NOTE_URL = `${this.BASE_URL}/notes/`;
 
 
   constructor(private http: HttpClient) {
@@ -30,7 +35,23 @@ export class ApiService {
     return this.http.post<Notebook>(this.SAVE_UPDATE_NOTEBOOK_URL, notebook);
   }
 
+  postNote(note:Note):Observable<Note>{
+    return this.http.post<Note>(this.SAVE_UPDATE_NOTE_URL, note);
+  }
+
   deleteNotebook(id:string):Observable<any>{
     return this.http.delete(this.DELETE_NOTEBOOK_URL + id);
+  }
+
+  deleteNote(id:string):Observable<any>{
+    return this.http.delete(this.DELETE_NOTE_URL + id);
+  }
+
+  getAllNotes():Observable<Note[]>{
+    return this.http.get<Note[]>(this.ALL_NOTES_URL);
+  }
+
+  getNotesByNotebook(notebookId:string):Observable<Note[]>{
+    return this.http.get<Note[]>(this.NOTES_BY_NOTEBOOK_URL + notebookId);
   }
 }
