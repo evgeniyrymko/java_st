756bc994ca9e89783b4abc6da69cbe0853aba351
evgeniyrymko <evgeniyrymko@gmail.com>
2020-06-26 10:13:01 +0300

    Rooms with bulbs getting started

---
+++ Rooms with bulbs/src/main/java/by/itsparnter/task/RoomsWithBulbsApplication.java
@@ -0,0 +1,13 @@
+package by.itsparnter.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsWithBulbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsWithBulbsApplication.class, args);
+	}
+
+}
---
+++ Rooms with bulbs/src/test/java/by/itsparnter/task/RoomsWithBulbsApplicationTests.java
@@ -0,0 +1,13 @@
+package by.itsparnter.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsWithBulbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
