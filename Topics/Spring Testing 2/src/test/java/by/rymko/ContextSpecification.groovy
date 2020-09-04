d34a3e7c1254925c0d54c2c092fda46f37017a6e
evgeniyrymko <evgeniyrymko@gmail.com>
2020-09-04 13:06:07 +0300

    Spring Testing 2 Context Specification TODO

--- Topics/Spring Testing 2/src/test/java/by/rymko/ContextSpecification.groovy
+++ Topics/Spring Testing 2/src/test/java/by/rymko/ContextSpecification.groovy
@@ -1,8 +1,26 @@
 package by.rymko
 
-
+import by.rymko.configuration.SpringConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.support.AnnotationConfigContextLoader
 import spock.lang.Specification
 
+@ContextConfiguration(classes = SpringConfiguration, loader = AnnotationConfigContextLoader)
 class ContextSpecification extends Specification {
+
+    @Autowired
+    private ApplicationContext applicationContext
+
+    def "two plus two should be equal four"() {
+        expect:
+        2 + 2 == 4
+    }
+
+    def "application context should be loaded"() {
+        expect:
+        applicationContext == null
+    }
 
 }
