20260ce541989fe498f549da73cc566cad19e431
evgeniyrymko <evgeniyrymko@gmail.com>
2020-09-08 10:23:29 +0300

    Spring MVC Testing xml configuration added

--- Topics/Spring Testing/src/main/java/by/rymko/music/ClassicalMusic.java
+++
@@ -1,9 +0,0 @@
-package by.rymko.music;
-
-public class ClassicalMusic implements Music {
-
-    @Override
-    public String getSong() {
-        return "Hungarian Rhapsody";
-    }
-}
--- Topics/Spring Testing/src/main/java/by/rymko/music/RockMusic.java
+++
@@ -1,8 +0,0 @@
-package by.rymko.music;
-
-public class RockMusic implements Music {
-    @Override
-    public String getSong() {
-        return "Wind cries Mary";
-    }
-}
